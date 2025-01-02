24e6c555aea8ccd13f652d18e52eed0942718445
aramyxt <truongaramy@gmail.com>
2025-01-02 14:44:42 -0800

    Added Sphinx Doc Files (WIP)
    Added files and build system for Sphinx documentation. Work in progress, will add more documentation on functions and quickstart guide.

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,23 @@
+.. Pore2Chip documentation master file, created by
+   sphinx-quickstart on Thu Jan  2 09:45:53 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Pore2Chip Documentation
+=======================
+
+**Pore2Chip** is a Python module designed to streamline the process of analyzing X-ray computed tomography (XCT) 
+images of soil and creating 2D micromodel designs based on that analysis. 
+It leverages the power of open-source libraries like OpenPNM, PoreSpy, and drawsvg to extract key information about the soil's 
+porous structure and translate it into a blueprint for microfluidic simulations or physical "lab-on-a-chip" devices developed using additive manufacturing.
+
+.. note::
+
+   This project is under active development.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   install
+
---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Pore2Chip'
+copyright = '2025, Aramy Truong, Maruti Mudunuru, Md Lal Mamud'
+author = 'Aramy Truong, Maruti Mudunuru, Md Lal Mamud'
+release = '0.1.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'nature'
+html_static_path = ['_static']
